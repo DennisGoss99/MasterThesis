1df169762d2b623533bcc4df42501763128fbd6e
DennisGoss99 <dennisgossler98@gmail.com>
2024-02-17 20:51:42 +0100

    fixed modelHelper

--- code/modelHelper.py
+++ code/modelHelper.py
@@ -1,17 +1,19 @@
 import torch
 import argparse
+import logging
+import datetime
 
 @torch.no_grad()
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-@torch.no_grad()
-def model_saveFile(version, epoch, train='pretrain'):
-    return f"tempModel/model{train}{version}_FloorEP{epoch}.pth"
+def model_saveFile(outputdir, version, epoch, train=''):
+    return f"{outputdir}/tempModel/model{train}{version}_EP{epoch}-{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.pth"
 
 def parseParameter():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', type=str, help='dataset path', required=True)
+    parser.add_argument('-o', '--out', type=str, help='output dir path', required=False, default='./')
     parser.add_argument('-d','--dataset', type=str, help='Select between: \"AllData_1080x\", \"AllData_512x\", \"CsGoFloor_1080x\", \"CsGoFloor_512x\", \"FreePBR\", \"Polyhaven\", \"Poliigon\"', required=True)
     parser.add_argument('-v', '--valsize', type=int, help='size of the validation dataset [default 500]', required=False , default=500)
     parser.add_argument('-i', '--iter', type=int, help='number of iterations [default 1]', required=False , default=1)
@@ -21,24 +23,41 @@ def parseParameter():
     args = parser.parse_args()
     return args
 
+
+
 @torch.no_grad()
-def write_parameter(device, dataset, train_size, valsize, repeatdataset, equalize, learning_rate, batch_size, image_size, block_size, channels_img, n_embd, n_head, n_layer, dropout, version):
-    path = f"tempModel/modelParameter{version}.txt"
-    with open(path, 'w') as file:
-        file.write(f'LEARNING_RATE={learning_rate}\n')
-        file.write(f'BATCH_SIZE={batch_size}\n')
-        file.write(f'IMAGE_SIZE={image_size}\n')
-        file.write(f'BLOCK_SIZE={block_size}\n')
-        file.write(f'CHANNELS_IMG={channels_img}\n')
-        file.write(f'N_EMBD={n_embd}\n')
-        file.write(f'N_HEAD={n_head}\n')
-        file.write(f'N_LAYER={n_layer}\n')
-        file.write(f'DROPOUT={dropout}\n')
-        file.write(f'VERSION={version}\n')
-        file.write(f'----------------\n')
-        file.write(f'device={device}\n')
-        file.write(f'dataset={dataset}\n')
-        file.write(f'train_size={train_size}\n')
-        file.write(f'valsize={valsize}\n')
-        file.write(f'repeatdataset={repeatdataset}\n')
-        file.write(f'equalize={equalize}\n')
+def logger_write_parameter(logger, device, dataset, train_size, valsize, repeatdataset, equalize, learning_rate, batch_size, image_size, block_size, channels_img, n_embd, n_head, n_layer, dropout, version, model_parameter):
+    logger.info(f'LEARNING_RATE={learning_rate}')
+    logger.info(f'BATCH_SIZE={batch_size}')
+    logger.info(f'IMAGE_SIZE={image_size}')
+    logger.info(f'BLOCK_SIZE={block_size}')
+    logger.info(f'CHANNELS_IMG={channels_img}')
+    logger.info(f'N_EMBD={n_embd}')
+    logger.info(f'N_HEAD={n_head}')
+    logger.info(f'N_LAYER={n_layer}')
+    logger.info(f'DROPOUT={dropout}')
+    logger.info(f'MODEL_PARAMETER={model_parameter}')
+    logger.info(f'VERSION={version}')
+    logger.info(f'----------------')
+    logger.info(f'device={device}')
+    logger.info(f'dataset={dataset}')
+    logger.info(f'train_size={train_size}')
+    logger.info(f'valsize={valsize}\n')
+    logger.info(f'repeatdataset={repeatdataset}')
+    logger.info(f'equalize={equalize}')
+    logger.info(f'----------------')
+
+
+def setup_logger(outputdir, version):
+    logger = logging.getLogger('model_logger')
+    logger.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(f"{outputdir}/tempModel/modelLog{version}-{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.log")    
+    stream_handler.setLevel(logging.DEBUG)
+    file_handler.setLevel(logging.DEBUG)
+
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+
+    return logger
