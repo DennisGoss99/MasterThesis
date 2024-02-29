5251f20c1d46dcf35144536806969204314ee7fe
Dennis Goßler <dennisgossler98@gmail.com>
2024-02-29 16:58:30 +0100

    add HalfLifeDataset

--- code/dataSetCombiner.py
+++ code/dataSetCombiner.py
@@ -10,7 +10,7 @@ def getDataSet(path, dataset_name, size_x, size_y, equalize = False, repeatData=
     equalizingPercentage = 0.10
 
     DataDic = {
-        "AllData_1080x": ["Data_Polyhaven1k", "Data_Poliigon", "Data_Minecraft/1024x1024", "Data_freepbr1k", "Data_CSGO_Floor", "Data_Portal"],
+        "AllData_1080x": ["Data_Polyhaven1k", "Data_Poliigon", "Data_Minecraft/1024x1024", "Data_freepbr1k", "Data_CSGO_Floor", "Data_Portal", "Data_HalfLife2"],
         "AllData_512x": ["Data_Minecraft/512x512", "Data_CSGO_Floor_qHD"],
         "Minecraft_1024x": ["Data_Minecraft/1024x1024"],
         "CsGoFloor_1080x": ["Data_CSGO_Floor"],
@@ -19,11 +19,18 @@ def getDataSet(path, dataset_name, size_x, size_y, equalize = False, repeatData=
         "Polyhaven": ["Data_Polyhaven1k"],
         "Poliigon": ["Data_Poliigon"],
         "Portal" : ["Data_Portal"],
+        "HalfLife2" : ["Data_HalfLife2"],
     }
 
-    # Prüfen, ob der angegebene Datensatzname im Wörterbuch vorhanden ist
+    specific_datasets = [
+        "Data_CSGO_Floor",
+        "Data_CSGO_Floor_qHD",
+        "Data_Portal",
+        "Data_HalfLife2"
+    ]
+
     if dataset_name not in DataDic:
-        raise ValueError(f"Datensatzname '{dataset_name}' nicht im DataDic gefunden.")
+        raise ValueError(f"Dataset'{dataset_name}' is not in DataDic")
 
     selected_data_paths = [f"{path}/{folder}" for folder in DataDic[dataset_name]]
 
@@ -40,7 +47,7 @@ def getDataSet(path, dataset_name, size_x, size_y, equalize = False, repeatData=
 
     for folder_path in selected_data_paths: 
         dataset = datasets.ImageFolder(root=folder_path, transform=transform)
-        if equalize and ("Data_CSGO_Floor" in folder_path or "Data_CSGO_Floor_qHD" in folder_path or "Data_Portal" in folder_path):
+        if equalize and any(specific_dataset in folder_path for specific_dataset in specific_datasets):
             subset_size = int(len(dataset) * equalizingPercentage)
             indices = np.random.choice(range(len(dataset)), subset_size, replace=False)
             dataset = Subset(dataset, indices)
